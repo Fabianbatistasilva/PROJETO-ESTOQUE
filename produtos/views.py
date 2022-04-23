a165849ce923aa1de5f09b882af14eb0ceb17b0a
JVwolfart <jvitor@pinheirasc.com>
2022-04-23 12:25:05 -0300

    JV 23/04 12:25

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,4 @@
+from math import prod
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.forms import PasswordChangeForm
@@ -6,7 +7,7 @@ from .models import Categoria, Historico, Medidas, Movimento, Produto
 from django.utils import timezone
 from datetime import datetime
 from django.core.paginator import Paginator
-from django.db.models import Count
+from django.db.models import Count, Sum
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -47,9 +48,10 @@ def alterar_senha(request):
 
 @login_required(login_url='login')
 def index(request):
+    user = request.user
     produtos = Produto.objects.all().filter(
-        ativo=True
-    )
+        ativo=True, usuario=user
+    ).order_by('-id')
     paginator = Paginator(produtos, 5)
     page = request.GET.get('p')
     produtos = paginator.get_page(page)
@@ -101,8 +103,18 @@ def entradas(request, id):
         return render(request, 'paginas/entradas.html', {'t_entrada':t_entrada, 'produto':produto, 'hoje':hoje})
     else:
         tipo = Movimento.objects.get(id=request.POST.get('mov'))
-        preco = float(request.POST.get('preco'))
-        quant = int(request.POST.get('quantidade'))
+        preco = request.POST.get('preco')
+        if preco == '':
+            preco = 0
+        else:
+            preco = float(preco)
+
+        quant = request.POST.get('quantidade')
+        if quant == '':
+            quant = 0
+        else:
+            quant = int(quant)
+            
         data = request.POST.get('data')
         if preco <= 0 and not tipo.somente_contabil:
             messages.add_message(request, messages.ERROR, 'Preço inválido, precisa ser positivo (diferente de zero)')
@@ -150,8 +162,17 @@ def saidas(request, id):
         return render(request, 'paginas/saidas.html', {'t_entrada':t_entrada, 'produto':produto, 'hoje':hoje, 'preco_venda':preco_venda})
     else:
         tipo = Movimento.objects.get(id=request.POST.get('mov'))
-        preco = float(request.POST.get('preco'))
-        quant = int(request.POST.get('quantidade'))
+        preco = request.POST.get('preco')
+        if preco == '':
+            preco = 0
+        else:
+            preco = float(preco)
+
+        quant = request.POST.get('quantidade')
+        if quant == '':
+            quant = 0
+        else:
+            quant = int(quant)
         data = request.POST.get('data')
         if preco <= 0 and not tipo.somente_contabil:
             messages.add_message(request, messages.ERROR, 'Preço inválido, precisa ser positivo (diferente de zero)')
@@ -159,7 +180,7 @@ def saidas(request, id):
         if preco < 0:
             messages.add_message(request, messages.ERROR, 'Preço inválido, precisa ser positivo (diferente de zero)')
             return render(request, 'paginas/saidas.html', {'t_entrada':t_entrada, 'produto':produto, 'hoje':hoje, 'preco_venda':preco_venda})
-        if quant <= 0:
+        if not quant or quant <= 0:
             messages.add_message(request, messages.ERROR, 'Quantidade inválida, precisa ser positivo (diferente de zero)')
             return render(request, 'paginas/saidas.html', {'t_entrada':t_entrada, 'produto':produto, 'hoje':hoje, 'preco_venda':preco_venda})
         if quant > produto.estoque:
@@ -266,6 +287,22 @@ def alterar_categoria(request, id):
             categoria.save()
             return redirect('adicionar_categoria')
 
+
+@login_required(login_url='login')
+def modal_categoria(request):
+    if request.method != 'POST':
+        return redirect('adicionar_produto')
+    else:
+        categ = request.POST.get('categoria').strip().title()
+        if len(categ) < 5:
+            messages.add_message(request, messages.ERROR, 'Descrição da categoria muito curta, mínimo 5 digitos')
+            return redirect('adicionar_produto')
+        else:
+            item = Categoria.objects.create(categoria=categ)
+            item.save()
+            messages.add_message(request, messages.SUCCESS, 'Nova categoria adicionada')
+            return redirect('adicionar_produto')
+
 @login_required(login_url='login')
 def adicionar_medida(request):
     medidas = Medidas.objects.all()
@@ -283,6 +320,21 @@ def adicionar_medida(request):
             item = Medidas.objects.create(descricao=categ)
             item.save()
             return redirect('adicionar_medida')
+
+@login_required(login_url='login')
+def modal_medida(request):
+    if request.method != 'POST':
+        return redirect('adicionar_produto')
+    else:
+        categ = request.POST.get('medida').strip().title()
+        if len(categ) == 0:
+            messages.add_message(request, messages.ERROR, 'Descrição da medida não pode ser vazia')
+            return redirect('adicionar_produto')
+        else:
+            item = Medidas.objects.create(descricao=categ)
+            item.save()
+            messages.add_message(request, messages.SUCCESS, 'Nova unidade de medida adicionada')
+            return redirect('adicionar_produto')
 
 @login_required(login_url='login')
 def alterar_medida(request, id):
@@ -350,18 +402,19 @@ def eliminar_historico(request, id):
         produto.estoque = produto.estoque + quant
     produto.save()
     historico.save()
-    messages.add_message(request, messages.INFO, 'Movimento eliminado com sucesso')
+    messages.add_message(request, messages.SUCCESS, 'Registro eliminado com sucesso')
     return redirect('relatorio', produto.id)
 
 @login_required(login_url='login')
 def pouco_estoque(request, quant):
     quant = request.GET.get('quant')
+    user = request.user
     if quant == None:
         quant=0
     else:
         quant = int(quant)
     produtos = Produto.objects.all().order_by('estoque').filter(
-        ativo=True
+        ativo=True, usuario=user
     ).exclude(estoque__gt=quant)
     paginator = Paginator(produtos, 5)
     page = request.GET.get('p')
@@ -371,12 +424,13 @@ def pouco_estoque(request, quant):
 @login_required(login_url='login')
 def muito_estoque(request, quant):
     quant = request.GET.get('quant')
+    user = request.user
     if quant == None:
         quant=10
     else:
         quant = int(quant)
     produtos = Produto.objects.all().order_by('-estoque').filter(
-        ativo=True, estoque__gt=quant-1
+        ativo=True, estoque__gt=quant-1, usuario=user
     )
     paginator = Paginator(produtos, 5)
     page = request.GET.get('p')
@@ -385,7 +439,8 @@ def muito_estoque(request, quant):
 
 @login_required(login_url='login')
 def por_categoria(request):
-    categorias = Categoria.objects.order_by('categoria').filter(produto__ativo=True).annotate( n=Count('produto'))
+    user = request.user
+    categorias = Categoria.objects.order_by('categoria').filter(produto__ativo=True, produto__usuario=user).annotate( n=Count('produto'))
     paginator = Paginator(categorias, 6)
     page = request.GET.get('p')
     categorias = paginator.get_page(page)
@@ -393,11 +448,90 @@ def por_categoria(request):
 
 @login_required(login_url='login')
 def produto_por_categoria(request, id):
+    user = request.user
     categoria = Categoria.objects.get(id=id)
     produtos = Produto.objects.all().filter(
-        ativo=True, categoria=categoria
+        ativo=True, categoria=categoria, usuario=user
     )
     paginator = Paginator(produtos, 6)
     page = request.GET.get('p')
     produtos = paginator.get_page(page)
     return render(request, 'paginas/produto_categoria.html', {'produtos':produtos, 'categoria':categoria})
+
+@login_required(login_url='login')
+def financeiro(request, id):
+    produto = Produto.objects.get(id=id)
+    if produto.preco_ultima_compra:
+        saida_comercial = Historico.objects.all().filter(
+            movimentacao__entrada=False, movimentacao__somente_contabil=False, excluido=False, produto=produto
+        )
+        total_item_sc = 0
+        total_valor_sc = 0
+        venda = 0
+        for c in saida_comercial:
+            total_item_sc += c.quantidade
+            venda = c.quantidade * c.preco
+            total_valor_sc += venda
+        entrada_comercial = Historico.objects.all().filter(
+            movimentacao__entrada=True, movimentacao__somente_contabil=False, excluido=False, produto=produto
+        )
+        total_item_ec = 0
+        total_valor_ec = 0
+        venda = 0
+        for c in entrada_comercial:
+            total_item_ec += c.quantidade
+            venda = c.quantidade * c.preco
+            total_valor_ec += venda
+        
+        saida_contabil = Historico.objects.all().filter(
+            movimentacao__entrada=False, movimentacao__somente_contabil=True, excluido=False, produto=produto
+        )
+        total_item_contabil = 0
+        total_valor_contabil = 0
+        venda = 0
+        for c in saida_contabil:
+            total_item_contabil += c.quantidade
+            venda = c.quantidade * c.preco
+            total_valor_contabil += venda
+
+        entrada_contabil = Historico.objects.all().filter(
+            movimentacao__entrada=True, movimentacao__somente_contabil=True, excluido=False, produto=produto
+        )
+        total_entrada_contabil = 0
+        total_entrada_valor_contabil = 0
+        venda = 0
+        for c in entrada_contabil:
+            total_entrada_contabil += c.quantidade
+            venda = c.quantidade * c.preco
+            total_entrada_valor_contabil += venda
+        if total_item_ec != 0:
+            preco_medio_compra = total_valor_ec/total_item_ec
+        else:
+            preco_medio_compra = 0
+        if total_item_sc != 0:
+            preco_medio_venda = total_valor_sc/total_item_sc
+        else:
+            preco_medio_venda = produto.preco_venda
+        preco_medio_compra = round(preco_medio_compra, 2)
+        preco_medio_venda = round(preco_medio_venda, 2)
+        if total_item_sc != 0 and total_item_ec != 0:
+            estimativa_lucro = -(preco_medio_compra*total_item_sc)+total_valor_sc
+            media_lucro_item = estimativa_lucro/total_item_sc
+        else:
+            estimativa_lucro = 0
+            media_lucro_item = produto.preco_venda-produto.preco_ultima_compra
+        media_lucro_item = round(media_lucro_item, 2)
+        if preco_medio_venda != 0 and preco_medio_compra != 0:
+            margem_lucro_pm = ((preco_medio_venda/preco_medio_compra)-1)*100
+        else:
+            margem_lucro_pm = 0
+        margem_lucro_pm = round(margem_lucro_pm, 4)
+        projecao_fat_estoque = produto.estoque*preco_medio_venda
+        projecao_lucro_estoque = produto.estoque*media_lucro_item
+        return render(request, 'paginas/financeiro.html', {'produto':produto, 'total_item_sc':total_item_sc,'total_valor_sc':total_valor_sc, 'total_item_ec':total_item_ec, 'total_valor_ec':total_valor_ec, 'total_item_contabil':total_item_contabil,
+        'total_valor_contabil':total_valor_contabil, 'total_entrada_contabil':total_entrada_contabil, 'total_entrada_valor_contabil':total_entrada_valor_contabil,
+        'preco_medio_compra':preco_medio_compra, 'preco_medio_venda':preco_medio_venda, 'estimativa_lucro':estimativa_lucro,
+        'media_lucro_item':media_lucro_item, 'margem_lucro_pm':margem_lucro_pm, 'projecao_fat_estoque':projecao_fat_estoque, 'projecao_lucro_estoque':projecao_lucro_estoque})
+    else:
+        messages.add_message(request, messages.ERROR, 'Não foi efetuado nenhuma compra desse item ainda, para acessar o relatório financeiro é necessário que exista alguma compra registrada')
+        return redirect('relatorio', id)
